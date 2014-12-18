ede783f8f6ce7d5b19297d6e9d1e3b6f9546f133
robneu <rob@wpsitecare.com>
2014-12-18 01:26:04 -0600

    Improved documentation and coding standards

--- dashboard-custom.php
+++ dashboard-custom.php
@@ -1,11 +1,26 @@
-<?php 
+<?php
+/**
+ * Redirect to our custom dashboard location.
+ *
+ * @package     VVVDashboard
+ * @copyright   Copyright (c) 2014, Leo Gopal
+ * @license     GPL-2.0+
+ * @since       1.0.0
+ */
 
-function redirect($url, $statusCode = 301)
-{
-   header('Location: ' . $url, true, $statusCode);
-   die();
+/**
+ * Redirect to another location using PHP.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  $url string the path of the redirect
+ * @param  $status_code integer stats code used when redirecting
+ * @return void
+ */
+function redirect( $url, $status_code = 301 ) {
+	header( 'Location: ' . $url, true, $status_code );
+	die();
 }
 
-redirect('/dashboard/index.php');
-
-?>
+// Fire our redirect.
+redirect( '/dashboard/index.php' );
