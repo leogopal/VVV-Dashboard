72113d6b8d3ba048ff761dd1d6295e4b85027bc8
Nabil Kadimi <nabil@kadimi.com>
2015-11-04 11:28:29 +0000

    Remove link to cracked website

--- dashboard/partials/footer.php
+++ dashboard/partials/footer.php
@@ -16,7 +16,6 @@
          		<p><img src="assets/img/dashboard-icon-x32.png"><code>v 1.2.0</code> | GPL+2 License | Leo Gopal ( <a href="https://twitter.com/leogopal" target="_blank">twitter</a> )</p>			</div>
 			<div class="large-6 columns">
 				<ul class="inline-list right">
-					<li><a href="http://dayofcode.net/" target="_blank">Day of Code</a></li>
 					<li><a href="https://github.com/leogopal/VVV-Dashboard" target="_blank">GitHub</a></li>
 					<li><a href="https://github.com/leogopal/VVV-Dashboard/issues/new" target="_blank">Submit Issue</a></li>
 					<li><a href="about.php">About</a></li>
